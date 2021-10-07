19e7de1558429713867d7846b08217d681a71867
Somberor <46875147+Somberor@users.noreply.github.com>
2021-10-07 19:24:33 +0300

    Json Parse

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 
     
@@ -6,6 +7,8 @@ import requests
 def main():
     print("Hello Mister!")
     x = requests.get('http://api.weatherapi.com/v1/current.json?key=baf9c1228bc647b19ab161704210710&q=London&aqi=no')
+    print("Loaded Json")
+    x = json.load(x)
     print("hello here is the wheater for London n' stuff" , x.text)
 
 if __name__ == "__main__":
